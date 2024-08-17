087a1bfa53550fad7c4e87db18e8afe8def11a42
ketan juikar <ketan27j@gmail.com>
2024-08-17 16:21:20 +0530

    Fix tailwindcss issue

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,17 +1,10 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "../providers";
 import { AppbarClient } from "../components/AppbarClient";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-});
+const inter = Inter({ subsets:["latin"]});
 
 export const metadata: Metadata = {
   title: "Whale Alert",
@@ -26,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Providers>
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={inter.className}>
         <div className="min-w-screen min-h-screen bg-[#ebe6e6]">
           < AppbarClient />
             {children}
