c2fa55fcaf1a0d0252492498a9cd758b1d2edaf0
ketan juikar <ketan27j@gmail.com>
2024-08-29 13:28:40 +0530

    Added transaction monitoring backend project

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,12 +1,61 @@
+import axios from "axios";
 import express from "express";
+import TwitterApi from "twitter-api-v2";
+import prisma from '@repo/db';
+import { checkForLargeTransaction, getLatestTransactions, insertTransaction } from "./utils/solana";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express()
 
-app.get("/", (req, res) => {
-    res.json({
-        message: "hello world"
+const twitterClient = new TwitterApi({
+    appKey: process.env.TWITTER_API_KEY || '',
+    appSecret: process.env.TWITTER_API_SECRET || '',
+    accessToken: process.env.TWITTER_ACCESS_TOKEN || '',
+    accessSecret: process.env.TWITTER_ACCESS_SECRET || '',
+  });
+  const rwClient = twitterClient.readWrite;
+
+app.get("/monitor", async (req, res) => {
+    try {
+        const response = await axios.get(process.env.COINGECKO_URL || 'https://api.coingecko.com/api/v3/simple/price?ids=solana&vs_currencies=usd')
+        const solPriceInUsd = response.data.solana.usd;
+        const wallets = await prisma.solanaWallet.findMany();
+        for (const wallet of wallets) {
+        const transactions = await getLatestTransactions(wallet.address);
+        for (const transaction of transactions) {
+            const amountTransferredUSD = checkForLargeTransaction(transaction, solPriceInUsd)
+            if (amountTransferredUSD !== 0 && amountTransferredUSD >= wallet.threshold) {
+                const message = `Whale Alert! A transaction of $${amountTransferredUSD} SOL was detected. https://solscan.io/tx/${transaction.transaction.signatures[0]}`;
+                const transactionTweeted = await prisma.transaction.findFirst({
+                    where: {
+                        signature: transaction.transaction.signatures[0],
+                        tweeted: true
+                    }
+                });
+                if(!transactionTweeted) {
+                    console.log(message);
+                    await rwClient.v2.tweet(message);
+                    await insertTransaction(wallet, transaction, amountTransferredUSD);
+                    }
+                } else {
+                    console.log('Transaction already tweeted');
+                }
+            }
+        }
+    res.status(200).json({
+        message: "Monitoring completed"
     });
-})
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({
+            message: "Internal server error",
+            error: error
+        });
+}});
 app.listen(3003, () => {
     console.log("server is running on port 3003");
 })
+
---
+++ apps/backend/src/utils/solana.ts
@@ -0,0 +1,80 @@
+import prisma from '@repo/db';
+import { Connection, PublicKey, ParsedTransactionWithMeta, SignaturesForAddressOptions, TransactionResponse,LAMPORTS_PER_SOL } from '@solana/web3.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const connection = new Connection(process.env.SOLANA_MAINNET || 'https://api.mainnet-beta.solana.com');
+console.log(process.env.SOLANA_MAINNET);
+
+export async function getLatestTransactions(walletAddress: string): Promise<ParsedTransactionWithMeta[]> {
+  const publicKey = new PublicKey(walletAddress);
+  const transactionList = await getSignaturesWithRetry(publicKey, { limit: 10 });
+  // console.log(transactionList);
+  const output: ParsedTransactionWithMeta[] = [];
+  const delay = (ms:number) => new Promise((resolve) => setTimeout(resolve, ms));
+  
+  for(const transaction of transactionList){
+    // await delay(1000);
+    const details = await connection.getParsedTransaction(transaction.signature)
+    if(details){
+        output.push(details);
+    }
+  }
+  return output;
+};
+
+async function getSignaturesWithRetry(publicKey:PublicKey, options:SignaturesForAddressOptions, retries = 5) {
+  const delay = (ms:number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+  for (let i = 0; i < retries; i++) {
+    try {
+      console.log(`Attempt ${i + 1} to get signatures for address ${publicKey.toString()}`);
+      const signatures = await connection.getSignaturesForAddress(publicKey, options);
+      return signatures;
+    } catch (error:any) {
+      if (error.message.includes('429')) {
+        const retryAfter = 11000; // Exponential backoff with jitter
+        console.log(`Rate limited. Retrying after ${retryAfter} ms...`);
+        await delay(retryAfter);
+      } else {
+        throw error;
+      }
+    }
+  }
+  throw new Error('Too many requests, please try again later.');
+}
+
+export const checkForLargeTransaction = (transactionDetails: ParsedTransactionWithMeta, solPriceInUsd: number): number => {
+  if (!transactionDetails || !transactionDetails.meta) return 0;
+
+  const preBalances = transactionDetails.meta.preBalances;
+  const postBalances = transactionDetails.meta.postBalances;
+  
+  if (!preBalances || !postBalances) return 0;
+
+  let amountTransferred = 0;
+  for (let i = 0; i < preBalances.length; i++) {
+    const balanceChange = (postBalances[i] ?? 0)-(preBalances[i] ?? 0);
+    if(Math.abs(balanceChange) > amountTransferred){
+      amountTransferred = balanceChange
+    }
+  }
+  
+  const amountTransferredSOL = (Math.abs(amountTransferred) / LAMPORTS_PER_SOL) ; 
+  const amountTransferredUSD = amountTransferredSOL * solPriceInUsd;
+  // console.log(`preBalances: ${preBalances} postBalances: ${postBalances} amountTransferredSOL: ${amountTransferredSOL} amountTransferredUSD: ${amountTransferredUSD}`);
+  return amountTransferredUSD;
+};
+
+export async function insertTransaction(wallet: { id: number; address: string; threshold: number; createdAt: Date; }, transaction:ParsedTransactionWithMeta, amountTransferredUSD: number) {
+  await prisma.transaction.create({
+    data: {
+      walletId: wallet.id,
+      amount: amountTransferredUSD,
+      signature: transaction.transaction.signatures[0] ?? '',
+      tweeted: true,
+      timestamp: new Date(),
+    },
+  });
+}
